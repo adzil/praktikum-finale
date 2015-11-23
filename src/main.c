f6fe358e0a6f721c88c22d84db3cb18258a52534
Adzil <adzil13@gmail.com>
2015-11-23 10:00:49 +0700

    Add includes

--- src/main.c
+++ src/main.c
@@ -1,32 +1,46 @@
-/*
-===============================================================================
- Name        : praktikum-finale.c
- Author      : $(author)
- Version     :
- Copyright   : $(copyright)
- Description : main definition
-===============================================================================
-*/
-
-#ifdef __USE_CMSIS
-#include "LPC17xx.h"
-#endif
-
-#include <cr_section_macros.h>
-
-// TODO: insert other include files here
-
-// TODO: insert other definitions and declarations here
+/* Main includes */
+#include "main.h"
 
 int main(void) {
+	init_ssp();
+	joystick_init();
+	oled_init();
+	oled_clearScreen(OLED_COLOR_BLACK);
 
-    // TODO: insert code here
-
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
     while(1) {
-        i++ ;
+
     }
-    return 0 ;
+
+    return 0;
+}
+
+static void init_ssp(void) {
+	SSP_CFG_Type SSP_ConfigStruct;
+	PINSEL_CFG_Type PinCfg;
+	/*
+	* Initialize SPI pin connect
+	* P0.7 - SCK;
+	* P0.8 - MISO
+	* P0.9 - MOSI
+	* P2.2 - SSEL - used as GPIO
+	*/
+	PinCfg.Funcnum = 2;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 0;
+	PinCfg.Pinnum = 7;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 8;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 9;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Funcnum = 0;
+	PinCfg.Portnum = 2;
+	PinCfg.Pinnum = 2;
+	PINSEL_ConfigPin(&PinCfg);
+	SSP_ConfigStructInit(&SSP_ConfigStruct);
+	// Initialize SSP peripheral with parameter given in structure above
+	SSP_Init(LPC_SSP1, &SSP_ConfigStruct);
+	// Enable SSP peripheral
+	SSP_Cmd(LPC_SSP1, ENABLE);
 }
