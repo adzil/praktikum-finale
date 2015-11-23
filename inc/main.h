651bbd3725b4058b2ee8e5380c4ac64ef44cf4d0
Adzil <adzil13@gmail.com>
2015-11-23 10:02:35 +0700

    Add include file for main.c

---
+++ inc/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/* Library includes */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* LPC-specific includes */
+#include <cr_section_macros.h>
+#include "LPC17xx.h"
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_timer.h"
+#include "lpc17xx_ssp.h"
+#include "joystick.h"
+#include "oled.h"
+
+/* FreeRTOS includes */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+int main (void);
+static void init_ssp(void);
+
+#endif /* MAIN_H_ */
